2d39f12f80bd2b2b578d3cb2de2916feeccf554f
Charles <cmacanka@gmail.com>
2011-06-13 22:24:58 -0400

    Added display_name method for Source class.

--- lib/PAPS/Database/papsdb/Schema/Result/Source.pm
+++ lib/PAPS/Database/papsdb/Schema/Result/Source.pm
@@ -157,6 +157,21 @@ __PACKAGE__->many_to_many(files => 'source_files', 'file',
                               '+as' => 'file_parent_url',
                           });
 
+
+=head1 Helper Methods
+
+=head2 display_name
+
+Returns a formatted version of the name suitable for display.
+
+=cut
+
+sub display_name {
+    my ($self) = @_;
+
+    return $self->name;
+}
+
 # You can replace this text with custom content, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
 1;
