10367cdae226a442f8860545ab67f1785a6ecd12
Charles <cmacanka@gmail.com>
2011-06-13 22:24:38 -0400

    Added display_name method for File class.

--- lib/PAPS/Database/papsdb/Schema/Result/File.pm
+++ lib/PAPS/Database/papsdb/Schema/Result/File.pm
@@ -179,5 +179,20 @@ __PACKAGE__->many_to_many(sources => 'source_files', 'source',
                           {  });
 
 
+=head1 Helper Methods
+
+=head2 display_name
+
+Returns a formatted version of the name suitable for display.
+
+=cut
+
+sub display_name {
+    my ($self) = @_;
+
+    return $self->work->display_name . ' (' . $self->file_type . ')';
+}
+
+
 __PACKAGE__->meta->make_immutable;
 1;
