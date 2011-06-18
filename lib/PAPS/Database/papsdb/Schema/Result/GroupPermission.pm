c52089316e45d67d802e5fb1267690057f679fb3
Charles <cmacanka@gmail.com>
2011-06-18 18:13:16 -0400

    Added display_name method for GroupPermission class.

--- lib/PAPS/Database/papsdb/Schema/Result/GroupPermission.pm
+++ lib/PAPS/Database/papsdb/Schema/Result/GroupPermission.pm
@@ -81,6 +81,20 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07002 @ 2011-06-12 13:38:59
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:MALLXi5F/kLvhEjAXoaBZw
 
+=head1 Helper Methods
+
+=head2 display_name
+
+Returns a formatted version of the name suitable for display.
+
+=cut
+
+sub display_name {
+    my ($self) = @_;
+
+    return $self->group->name . " - " . $self->permission->name;;
+}
+
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
