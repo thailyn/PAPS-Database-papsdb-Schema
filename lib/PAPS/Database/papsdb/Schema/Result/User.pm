673a2c8e59fccd69dc2a5fb33402d59abc30d495
Charles <cmacanka@gmail.com>
2011-06-18 18:17:17 -0400

    Added display_name method for User class.

--- lib/PAPS/Database/papsdb/Schema/Result/User.pm
+++ lib/PAPS/Database/papsdb/Schema/Result/User.pm
@@ -174,6 +174,19 @@ __PACKAGE__->has_many(
 # Created by DBIx::Class::Schema::Loader v0.07002 @ 2011-06-12 20:44:13
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:rctGBJdbxtG4sCASIxgc9w
 
+=head1 Helper Methods
+
+=head2 display_name
+
+Returns a formatted version of the name suitable for display.
+
+=cut
+
+sub display_name {
+    my ($self) = @_;
+
+    return $self->name;
+}
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
