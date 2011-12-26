b4595d09824d0894f481121c5923dabef92c65de
Charles <cmacanka@gmail.com>
2011-12-26 16:21:32 -0500

    Added SourceCategory and SourceCategoryType display_name methods.

--- lib/PAPS/Database/papsdb/Schema/Result/SourceCategory.pm
+++ lib/PAPS/Database/papsdb/Schema/Result/SourceCategory.pm
@@ -220,5 +220,25 @@ __PACKAGE__->many_to_many(mapped_categories => 'category_mappings',
                           'category',
                           { });
 
+=head1 Helper Methods
+
+=head2 display_name
+
+Returns a formatted version of the name suitable for display.
+
+=cut
+
+sub display_name {
+    my ($self) = @_;
+
+    #my $value = $self->name;
+    #$value .= " (" . $self->source.display_name() . ")";
+    ##my $value = ($self->name() . " (" . ($self->source.display_name() || "Unknown") . ")");
+
+    #return ($self->name . " (" . $self->source.display_name() . ")");
+    return $self->name;
+    #return $value;
+}
+
 __PACKAGE__->meta->make_immutable;
 1;
--- lib/PAPS/Database/papsdb/Schema/Result/SourceCategoryType.pm
+++ lib/PAPS/Database/papsdb/Schema/Result/SourceCategoryType.pm
@@ -111,6 +111,25 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07002 @ 2011-07-24 16:54:51
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:qGKhyYPZuKTy+7/4e0twdQ
 
+=head1 Helper Methods
+
+=head2 display_name
+
+Returns a formatted version of the name suitable for display.
+
+=cut
+
+sub display_name {
+    my ($self) = @_;
+
+    #my $value = $self->name;
+    #$value .= " (" . $self->source.display_name() . ")";
+    ##my $value = ($self->name() . " (" . ($self->source.display_name() || "Unknown") . ")");
+
+    #return ($self->name . " (" . $self->source.display_name() . ")");
+    return $self->name;
+    #return $value;
+}
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
