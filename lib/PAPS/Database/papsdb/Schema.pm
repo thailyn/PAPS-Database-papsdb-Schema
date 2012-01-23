da6389cdb1c74df1527f8a43af5bd859959e1cc1
Charles <cmacanka@gmail.com>
2012-01-22 21:57:15 -0500

    Advanced version to 0.004.

--- lib/PAPS/Database/papsdb/Schema.pm
+++ lib/PAPS/Database/papsdb/Schema.pm
@@ -13,7 +13,7 @@ __PACKAGE__->load_namespaces;
 
 # Created by DBIx::Class::Schema::Loader v0.07015 @ 2012-01-15 22:01:14
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:IiTLQhRYCprmVOKCn/SnPQ
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 $VERSION = eval $VERSION;
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
